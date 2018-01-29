cbdadc44e787b1125770b53299a091e1b48e3b13
John Homan <jhoman@johns-mbp.resource.campus.njit.edu>
2018-01-29 16:52:07 -0500

    updated test cases for primitive arrays

--- unit_test/primitive_arrays/PrimitiveArrayWriter.java
+++ unit_test/primitive_arrays/PrimitiveArrayWriter.java
@@ -71,13 +71,16 @@ class PrimitiveArrayWriter {
         double []double_array_new = new double[300];
         fos = new FileOutputStream("double_array_new.ser");
         oos = new ObjectOutputStream(fos);
-        oos.writeObject(double_array_empty);
+        oos.writeObject(double_array_new);
+        System.out.println(double_array_new.length);
         fos.close();
         oos.close();
 
         byte []byte_array_signed = {};
         byte []byte_array_unsigned = {};
         byte []byte_array_limits = {Byte.MAX_VALUE, 0, Byte.MAX_VALUE};
+        byte []byte_array_empty = {};
+        byte []byte_array_new = new byte[500];
 
         char []char_array_signed = {};
         char []char_array_unsigned = {};
@@ -87,13 +90,85 @@ class PrimitiveArrayWriter {
         long []long_array_unsigned = {-97609L, -906987806L, -79855734652564357L, -987809L};
         long []long_array_limits = {Long.MAX_VALUE, Long.MIN_VALUE, 0};
 
-        short []short_array_signed = {};
-        short []short_array_unsigned = {};
+        fos = new FileOutputStream("long_array_signed.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(long_array_signed);
+        fos.close();
+        oos.close();
+        
+        fos = new FileOutputStream("long_array_unsigned.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(long_array_unsigned);
+        fos.close();
+        oos.close();
+
+        fos = new FileOutputStream("long_array_limits.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(long_array_limits);
+        fos.close();
+        oos.close();
+        
+        
+
+        short []short_array_signed = {-(short)12321, -(short)123, -(short)22222, -(short)1232, (short)0};
+        short []short_array_unsigned = {(short)0, (short)121, (short)2323, (short)3343, (short)3234, (short)2123, (short)234};
         short []short_array_limits = {Short.MAX_VALUE, 0, Short.MIN_VALUE};
+        short []short_array_new = new short[10000];
+        short []short_array_empty = {};
+
+        fos = new FileOutputStream("short_array_signed.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(short_array_signed);
+        fos.close();
+        oos.close();
+
+        fos = new FileOutputStream("short_array_unsigned.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(short_array_unsigned);
+        fos.close();
+        oos.close();
+
+        fos = new FileOutputStream("short_array_limits.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(short_array_limits);
+        fos.close();
+        oos.close();
+
+        fos = new FileOutputStream("short_array_new.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(short_array_new);
+        fos.close();
+        oos.close();
+
+        fos = new FileOutputStream("short_array_empty.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(short_array_empty);
+        fos.close();
+        oos.close();
+
 
         boolean []boolean_array_true = {true, true, true, true, true, true};
         boolean []boolean_array_false = {false};
         boolean []boolean_array_mixed = {true, false, true, true, false};
+
+        fos = new FileOutputStream("boolean_array_true.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(boolean_array_true);
+        fos.close();
+        oos.close();
+
+        fos = new FileOutputStream("boolean_array_false.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(boolean_array_false);
+        fos.close();
+        oos.close();
+
+        fos = new FileOutputStream("boolean_array_mixed.ser");
+        oos = new ObjectOutputStream(fos);
+        oos.writeObject(boolean_array_mixed);
+        fos.close();
+        oos.close();
+
 
     }
 
